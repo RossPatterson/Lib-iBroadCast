a900f119ea45c160fc9fc7fa814b6d3387456004
Ross Patterson <ross.patterson@gmail.com>
2021-11-07 20:50:26 -0500

    Use typing in CLI

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import glob
 from lib_ibroadcast import ciBroadCast
 import logging
 import os
+from typing import Dict
+from typing import List
 
-logger = logging.getLogger('iBroadcast_CLI')
+logger:logging.Logger = logging.getLogger('iBroadcast_CLI')
 
-def setup_subcommands():
-    def add_common_args(parser):
+def setup_subcommands() -> argparse.ArgumentParser:
+    """
+    Set up the argument parser and the handlers for each subcommand.
+    """
+    def add_common_args(parser:argparse.ArgumentParser) -> None:
+        """
+        Add all the arguments to a the parser that every parser should have.
+        """
         parser.add_argument("-d", "--dryrun", action="store_true", default=False, help="do not actually make changes")
         parser.add_argument("-p", "--password", help="password for iBroadcast account")
         parser.add_argument("-u", "--username", required=True, help="username for iBroadcast account")
         parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Verbose logging")
 
-    parser = argparse.ArgumentParser(description="iBroadcast CLI")
+    parser:argparse.ArgumentParser = argparse.ArgumentParser(description="iBroadcast CLI")
     parser.add_argument("-V", "--version", action="version", version = f"{parser.prog} version 1.0.0")
     parser.set_defaults(verbose=False)
 
-    subparsers = parser.add_subparsers(dest="operation")
+    subparsers:argparse.ArgumentParser = parser.add_subparsers(dest="operation")
     subparsers.add_parser("help")
 
-    parser_cp = subparsers.add_parser("create_playlist", aliases=["cp"])
+    parser_cp:argparse.ArgumentParser = subparsers.add_parser("create_playlist", aliases=["cp"])
     parser_cp.set_defaults(operation_func = create_playlist)
     add_common_args(parser_cp)
     parser_cp.add_argument("-D", "--description", required=True, help="Playlist description")
@@ -30,17 +38,17 @@ def setup_subcommands():
     parser_cp.add_argument("--public", dest="public", action="store_true", help="Create a public playlist")
     parser_cp.add_argument("--private", dest="public", action="store_false", default=False, help="Create a private playlist (default)")
 
-    parser_sa = subparsers.add_parser("select_album", aliases=["sa"])
+    parser_sa:argparse.ArgumentParser = subparsers.add_parser("select_album", aliases=["sa"])
     parser_sa.set_defaults(operation_func = select_albums_by_albumname)
     add_common_args(parser_sa)
     parser_sa.add_argument("-F", "--filter", required=True, help="Filter to apply")
 
-    parser_sf = subparsers.add_parser("select_folder", aliases=["sf"])
+    parser_sf:argparse.ArgumentParser = subparsers.add_parser("select_folder", aliases=["sf"])
     parser_sf.set_defaults(operation_func = select_tracks_by_folder)
     add_common_args(parser_sf)
     parser_sf.add_argument("-f", "--folder", required=True, help="Folder to select from")
 
-    parser_uf = subparsers.add_parser("upload_folder", aliases=["uf"])
+    parser_uf:argparse.ArgumentParser = subparsers.add_parser("upload_folder", aliases=["uf"])
     parser_uf.set_defaults(operation_func = upload_folder)
     add_common_args(parser_uf)
     parser_uf.add_argument("-f", "--folder", required=True, help="Folder to upload from")
@@ -49,27 +57,32 @@ def setup_subcommands():
 
     return parser
 
-def create_playlist(args):
+def create_playlist(args:argparse.Namespace) -> None:
     if not args.dryrun:
         ibroadcast_api.CreatePlayList(uName=args.name, uDescription=args.description, bPublic=args.public)
 
-def select_albums_by_albumname(args):
+def select_albums_by_albumname(args:argparse.Namespace) -> None:
     if not args.dryrun:
         ibroadcast_api.SelectAlbumsByAlbumName(uFilter=args.filter)
 
-def select_tracks_by_folder(args):
+def select_tracks_by_folder(args:argparse.Namespace) -> None:
     if not args.dryrun:
         ibroadcast_api.SelectTracksByFolder(uFolder=args.folder)
 
-def upload_folder(args):
-    def list_files(folder, supported_filetypes, recursive, verbose):
+def upload_folder(args:argparse.Namespace) -> None:
+    """
+    Update all the songs in a folder.
+    """
+    def list_files(folder:str, supported_filetypes:List[str], recursive:bool, verbose:bool) -> List[str]:
         """
         Enumerate all files in the folder that match the supported extension list, possbly recursing
         into subfolders, and ignoring "hidden" files (".whatever").
         """
-        files = []
+        ext:str
+        files:str = []
+        filename:str
         for filename in glob.glob(os.path.join(folder, '*')):
-            basename = os.path.basename(filename)
+            basename:str = os.path.basename(filename)
             if basename.startswith('.'):
                 if verbose:
                     logger.debug(f"Skipping {filename} - 'hidden' file")
@@ -80,32 +93,33 @@ def upload_folder(args):
                         logger.debug(f"Recursing into {filename}")
                     files += list_files(filename, supported_filetypes, recursive, verbose)
                 continue
-            dummy, ext = os.path.splitext(basename)
+            _, ext = os.path.splitext(basename)
             if ext not in supported_filetypes:
                 if verbose:
                     logger.debug(f"Skipping {filename} - not a supported filetype")
                 continue
             files.append(filename)
         return files
+    filename:str
     if args.verbose:
         logger.debug(f"Uploading tracks from {args.folder}{' recursively' if args.recursive else ''}")
-    supported_filetypes = ibroadcast_api.GetSupportedFiletypes()
-    folder_files = list_files(args.folder, supported_filetypes, args.recursive, args.verbose)
+    supported_filetypes:Dict = ibroadcast_api.GetSupportedFiletypes()
+    folder_files:List[str] = list_files(args.folder, supported_filetypes, args.recursive, args.verbose)
     for filename in folder_files:
         if args.verbose:
             logger.debug(f"Uploading {filename}")
         if not args.dryrun:
-            success = ibroadcast_api.UploadTrack(filename, bForce=args.force)
+            success:bool = ibroadcast_api.UploadTrack(filename, bForce=args.force)
             if not success:
                 logger.info(f"Upload of {filename} failed.")
 
 if __name__ == '__main__':
-    log_level = logging.INFO
+    log_level:int = logging.INFO
     logger.setLevel(log_level)
     logger.addHandler(logging.StreamHandler())
 
-    parser = setup_subcommands()
-    args = parser.parse_args()
+    parser:argparse.ArgumentParser = setup_subcommands()
+    args:argparse.Namespace = parser.parse_args()
     if args.verbose:
         log_level = logging.DEBUG
         logger.setLevel(log_level)
@@ -116,8 +130,7 @@ if __name__ == '__main__':
         args.password = getpass.getpass(f"Enter password for {args.username}: ")
     if args.dryrun:
         logger.info("Changes are suppressed by --dryrun")
-    ibroadcast_api = ciBroadCast(bAllowUndocumentedAPIs=True, iLogLevel=log_level)
+    ibroadcast_api:ciBroadCast = ciBroadCast(bAllowUndocumentedAPIs=True, iLogLevel=log_level)
     ibroadcast_api.Login(uUserName=args.username, uPassword=args.password)
     args.operation_func(args)
     ibroadcast_api.Logout()
-
